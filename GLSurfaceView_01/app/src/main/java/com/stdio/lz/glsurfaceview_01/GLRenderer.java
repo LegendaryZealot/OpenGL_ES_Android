bbc08b3dc695b154169e3a81565373c0f71786f4
李桩 <lz@lizhuangdeMacBook-Pro.local>
2018-10-07 12:26:14 +0800

    GLSurfaceView_01

---
+++ GLSurfaceView_01/app/src/main/java/com/stdio/lz/glsurfaceview_01/GLRenderer.java
@@ -0,0 +1,58 @@
+package com.stdio.lz.glsurfaceview_01;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GLRenderer implements GLSurfaceView.Renderer {
+
+    private Context context;
+    private int aPositionHandle;
+    private int programId;
+    private FloatBuffer vertexBuffer;
+    private final float[] vertexData = {
+            0f,0f,0f,
+            1f,-1f,0f,
+            1f,1f,0f
+    };
+
+    public  GLRenderer(Context context){
+        this.context=context;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        String vertexShader = Utilty.readRawTextFile(context, R.raw.vertex_shader);
+        String fragmentShader= Utilty.readRawTextFile(context, R.raw.fragment_shader);
+        programId=Utilty.createProgram(vertexShader,fragmentShader);
+        aPositionHandle= GLES20.glGetAttribLocation(programId,"aPosition");
+        vertexBuffer = ByteBuffer.allocateDirect(vertexData.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(vertexData);
+        vertexBuffer.position(0);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear( GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUseProgram(programId);
+        GLES20.glEnableVertexAttribArray(aPositionHandle);
+        GLES20.glVertexAttribPointer(aPositionHandle, 3, GLES20.GL_FLOAT, false,
+                12, vertexBuffer);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+    }
+
+}
---
+++ GLSurfaceView_01/app/src/main/java/com/stdio/lz/glsurfaceview_01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.stdio.lz.glsurfaceview_01;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private  GLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        glSurfaceView= (GLSurfaceView) findViewById(R.id.surface_view);
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(new GLRenderer(this));
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+}
